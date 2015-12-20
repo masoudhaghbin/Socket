8b1aabf51e25bf77eb87d755338ec070f4ad51e1
Masoud Hb <masoud.haghbin7@gmail.com>
2015-12-20 21:31:49 +0330

    Now its time to streaming
    changing client Architecture

--- Server.py
+++ Server.py
@@ -7,6 +7,8 @@ serversocket = socket(AF_INET , SOCK_STREAM )
 serversocket.bind((gethostname(), serverport))
 serversocket.listen(3)
 print "server is running "
+
+# --------------------------------------------------------------------------------------
 def register(name , socket , addr):
     flag = True
     global RegisteredUsers
@@ -22,6 +24,8 @@ def register(name , socket , addr):
         print "Registerd users :" ,
         print RegisteredUsers
     return flag
+# ---------------------------------------------------------------------------------------
+
 def Bye(addr):
     flag = False
     global RegisteredUsers
@@ -34,7 +38,30 @@ def Bye(addr):
         else:
             continue
     return flag 
+# ---------------------------------------------------------------------------------------
+def StreamRequest(addr , Filename):
+    print "We are in Stream Request function !"
+    flag = False
+    requesterName = ""
+    for i in range(len(RegisteredUsers)):
+        if(RegisteredUsers[i][2] == addr):
+            requesterName = RegisteredUsers[i][0]
+            flag = True
+            break
+        else:
+            continue
+    print requesterName , "requested the file"
+    if(flag):
+        for i in range(len(RegisteredUsers)):
+            if(RegisteredUsers[i][0] != requesterName):
+                RegisteredUsers[i][1].send("hey bitch !")
+            else:
+                continue
+    else:
+        return flag
 
+
+# ---------------------------------------------------------------------------------------
 def main(connectedsocket , addr):
     while True:
         print "in main"
@@ -59,12 +86,15 @@ def main(connectedsocket , addr):
                 print "RegisteredUsers are :" ,RegisteredUsers
             else:
                 connectedsocket.send("Reg#NOK#NotFoundInUsers")
+        elif(clientCommand[0:10] == "StreamReq#"):
+            Filename = clientCommand[10:]
+            addr = connectedsocket.getpeername()
+            StreamRequest(addr , Filename)
         else:
             connectedsocket.send("Sorry! Not right now !")
         # connectedsocket.close()
         # print RegisteredUsers
-   
-
+# ----------------------------------------------------------------------------------------
 
 while True:
         connectionsocket , addr = serversocket.accept()
