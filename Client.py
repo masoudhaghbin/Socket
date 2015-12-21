922b045befc5c0aec7347c2fbb74d7abc64d7232
Masoud Hb <masoud.haghbin7@gmail.com>
2015-12-21 13:29:34 +0330

    Simple Chaining OK!
    chaining is ok now
    stream request uses filename
    but next ones don't have the file yet !

--- Client.py
+++ Client.py
@@ -2,13 +2,12 @@ from socket import *
 import threading
 from random import randint
 
-
+filename = ""
 servername = gethostname()
 serverport = 12000
 UDP_Port = randint(12001 , 20000)
 clientsocket = socket(AF_INET , SOCK_STREAM)
 clientsocket.connect((servername , serverport))
-
 clientUDPsocket = socket(AF_INET , SOCK_DGRAM )
 clientUDPsocket.bind((servername, UDP_Port))
 
@@ -19,7 +18,17 @@ def listen(Mysocket):
 		print "From Server : " , servermessage
 		if(servermessage[0:10] == "NewStream#"):
 			print "new Stream Recieved !"
-			print "Do You Want this file ? " 
+			print "Do You Want this file ? "
+		elif(servermessage[0:10] == "StreamReq#"):
+			 print "Here :" , servermessage 
+		elif(servermessage[0:7] == "Stream#"):
+			# print "Im here !"
+			Next_UDP_Port = int(servermessage[7:])
+			# print "Next port is ", Next_UDP_Port
+			myFile = open(filename , 'rb')
+			message = myFile.read()
+			clientUDPsocket.sendto(message,(servername , Next_UDP_Port))
+			# print "sent!"
 			# choice = raw_input("Do you want the file ?")
 			# print "this is my choice :" , choice
 		
@@ -32,7 +41,14 @@ def listen(Mysocket):
 # 		Command = raw_input(option)
 # 		Mysocket.send(Command)
 def UDPfunc():
-	print "Thread running"
+	while True:
+	# print "Thread running"
+		modifiedmessage , serveraddress = clientUDPsocket.recvfrom(2048)
+		# filename_got = str(UDP_Port)+filename
+		# gotfile = open(filename_got , 'wb')
+		# gotfile.write(modifiedmessage)
+		print modifiedmessage
+		clientsocket.send("GOT#")
 
 threading.Thread(target=listen , args=(clientsocket,)).start()
 threading.Thread(target=UDPfunc , args=()).start()
@@ -44,6 +60,7 @@ while True:
 	elif(Command[0:3] == "Bye"):
 		clientsocket.send(Command)
 	elif(Command[0:10] == "StreamReq#"):
+		filename = Command[10:]
 		clientsocket.send(Command)
 	elif(Command[0:4] == "YEAH"):
 		print "UDP port is :" , UDP_Port
--- Server.py
+++ Server.py
@@ -4,7 +4,11 @@ import threading
 RegisteredUsers = []
 Demanders = []
 index = 0
+chainindex = 0
+CanbeStream = 1
+StreamDone = 1
 requesterName = ""
+requesterSocket = ""
 serverport = 12000
 serversocket = socket(AF_INET , SOCK_STREAM )
 serversocket.bind((gethostname(), serverport))
@@ -46,6 +50,8 @@ def StreamRequest(addr , Filename):
     print "We are in Stream Request function !"
     flag = False
     global requesterName
+    global CanbeStream
+    global StreamDone
     for i in range(len(RegisteredUsers)):
         if(RegisteredUsers[i][2] == addr):
             requesterName = RegisteredUsers[i][0]
@@ -55,6 +61,8 @@ def StreamRequest(addr , Filename):
             continue
     print requesterName , "requested the file"
     if(flag):
+        CanbeStream = 0
+        StreamDone = 0
         for i in range(len(RegisteredUsers)):
             if(RegisteredUsers[i][0] != requesterName):
                 RegisteredUsers[i][1].send("NewStream#"+Filename)
@@ -67,8 +75,10 @@ def StreamRequest(addr , Filename):
 # ---------------------------------------------------------------------------------------
 def startChainingProcess(requesterSocket):
     print "we are in this function with this Socket !"
-    print requesterName
-    print requesterSocket
+    # print requesterName
+    # print requesterSocket
+    global chainindex
+    requesterSocket.send("Stream#"+str(Demanders[chainindex]))
 # ---------------------------------------------------------------------------------------
 def main(connectedsocket , addr):
     while True:
@@ -76,6 +86,10 @@ def main(connectedsocket , addr):
         global RegisteredUsers
         global Demanders
         global index
+        global chainindex
+        global CanbeStream
+        global StreamDone
+        global requesterSocket
         clientCommand = connectedsocket.recv(1024)
         if(clientCommand[0:4] == "Reg#"):
             Clientname = clientCommand[4:]
@@ -97,27 +111,70 @@ def main(connectedsocket , addr):
             else:
                 connectedsocket.send("Reg#NOK#NotFoundInUsers")
         elif(clientCommand[0:10] == "StreamReq#"):
-            Filename = clientCommand[10:]
-            addr = connectedsocket.getpeername()
-            StreamRequest(addr , Filename)
+            if(CanbeStream == 1):
+                # CanbeStream = 0
+                # StreamDone = 0
+                Filename = clientCommand[10:]
+                requesterSocket = connectedsocket
+                # for i in range(len(RegisteredUsers)):
+                #     if(RegisteredUsers[i][1] != requesterName):
+                #         continue
+                #     else:
+                #         requesterSocket = RegisteredUsers[i][1]
+                #         break
+                addr = connectedsocket.getpeername()
+                StreamRequest(addr , Filename)
+            else:
+                connectedsocket.send("StreamReq#NOK#SERVERBUSYNOW")
         elif(clientCommand[0:4] == "YEAH"):
-            ClientUdpPort = clientCommand[5:]
-            print "client with port" , ClientUdpPort , "accepted !"
-            Demanders.append(ClientUdpPort)
-            index += 1
-            requesterSocket = ""
-            if(index == len(RegisteredUsers)-1 ):
-                for i in range(len(RegisteredUsers)):
-                    if(RegisteredUsers[i][0] != requesterName):
-                        continue
+            if(connectedsocket!=requesterSocket):
+                ClientUdpPort = clientCommand[5:]
+                print "client with port" , ClientUdpPort , "accepted !"
+                Demanders.append(ClientUdpPort)
+                index += 1
+                # requesterSocket = ""
+                if(index == len(RegisteredUsers)-1):
+                    if(len(Demanders)!=0 ):
+                        # for i in range(len(RegisteredUsers)):
+                        #     if(RegisteredUsers[i][0] != requesterName):
+                        #         continue
+                        #     else:
+                        #         requesterSocket = RegisteredUsers[i][1]
+                        #         break
+                        startChainingProcess(requesterSocket)
                     else:
-                        requesterSocket = RegisteredUsers[i][1]
-                        break
-                startChainingProcess(requesterSocket)
-            # connectedsocket.send("ok , I will send U!")
+                        CanbeStream = 1
+                        StreamDone = 1
+                        del Demanders[:]
+                        index = 0
+                        chainindex = 0
+                        requesterSocket.send("StreamReq#NOK#NoOneWants")
+                # connectedsocket.send("ok , I will send U!")
         elif(clientCommand[0:4] == "NOPE"):
-            index += 1
-            connectedsocket.send("ok , I wont send U!")
+            if(connectedsocket!=requesterSocket):
+                index += 1 
+                if(index == len(RegisteredUsers) -1):
+                    if(len(Demanders)!=0 ):
+                        startChainingProcess(requesterSocket)
+                    else:
+                        CanbeStream = 1
+                        StreamDone = 1
+                        del Demanders[:]
+                        index = 0
+                        chainindex = 0
+                        requesterSocket.send("StreamReq#NOK#NoOneWants") 
+                connectedsocket.send("ok , I wont send U!")
+        elif(clientCommand[0:4] == "GOT#"):
+            if(chainindex<len(Demanders) -1 ):
+                chainindex+=1
+                connectedsocket.send("Stream#"+Demanders[chainindex])
+            else:
+                requesterSocket.send("StreamReq#DONE")
+                del Demanders[:]
+                index = 0
+                chainindex = 0
+                CanbeStream = 1
+                StreamDone = 1
         else:
             connectedsocket.send("Sorry! Not right now !")
         # connectedsocket.close()
